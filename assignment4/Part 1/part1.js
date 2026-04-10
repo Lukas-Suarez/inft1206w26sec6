77de6f04dae084293eb4abe562d0c33e4d9f3bcf
Lukas-Suarez <lukassuarez633@gmail.com>
2026-04-10 16:23:40 -0400

    assignment 4 part 1 done
    assignment 4 part 1 done and the original file for part 2

--- assignment4/Part 1/part1.js
+++ assignment4/Part 1/part1.js
@@ -3,7 +3,7 @@
 //Date: 2026-04-10
 
 
-// Complete variable definitions and random functions
+// Complete variable definitions and random function
 
 const customName = document.getElementById("custom-name");
 const generateBtn = document.querySelector(".generate");
@@ -14,43 +14,47 @@ function randomValueFromArray(array) {
   return array[random];
 }
 
-// Raw text strings
+// Solution: Raw text strings
 
-// Willy the Goblin
-// Big Daddy
-// Father Christmas
+const characters = ["Jesus Christ", "Santa claws", "Jack Daniels"];
+const places = ["the crack den", "astro world", "antarctica"];
+const events = [
+  "got very itchy",
+  "lost bladder control",
+  "had a coming to god moment",
+];
 
-// the soup kitchen
-// Disneyland
-// the White House
-
-// spontaneously combusted
-// melted into a puddle on the sidewalk
-// turned into a slug and slithered away
-
-// Partial return random string function
+// Solution: Partial return random string function
 
 function returnRandomStoryString() {
-  // It was 94 Fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.
+  const randomCharacter = randomValueFromArray(characters);
+  const randomPlace = randomValueFromArray(places);
+  const randomEvent = randomValueFromArray(events);
+
+  let storyText = `It was 94 Fahrenheit outside, so ${randomCharacter} went out to explore. When they arrived at ${randomPlace}, something strange happened they ${randomEvent}. Bob saw everything unfold and wasnt too shocked, since ${randomCharacter} weighs 300 pounds and weird things always seem to happen around them.`;
 
   return storyText;
 }
 
-// Event listener and partial generate function definition
+// Solution: Event listener and partial generate function definition
 
 generateBtn.addEventListener("click", generateStory);
 
 function generateStory() {
+  let newStory = returnRandomStoryString();
+
   if (customName.value !== "") {
     const name = customName.value;
+    newStory = newStory.replaceAll("Bob", name);
   }
 
   if (document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature = Math.round(94);
+    const weight = `${Math.round(300 / 14)} stone`;
+    const temperature = `${Math.round((94 - 32) * (5 / 9))} Celsius`;
+    newStory = newStory.replace("300 pounds", weight);
+    newStory = newStory.replace("94 Fahrenheit", temperature);
   }
 
-  // TODO: replace "" with the correct expression
-  story.textContent = "";
+  story.textContent = newStory;
   story.style.visibility = "visible";
 }
