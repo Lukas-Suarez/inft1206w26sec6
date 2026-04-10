e88c41145da5a52d19c6fb826f3d147f120d19e4
Lukas-Suarez <lukassuarez633@gmail.com>
2026-04-10 16:25:21 -0400

    Assignmentt 4 part 2
    Comment headers added

--- assignment4/part 2/part2.js
+++ assignment4/part 2/part2.js
@@ -1,3 +1,7 @@
+//Name: Lukas Suarez
+//File: part2.js
+//Date: 2026-04-10
+
 const displayedImage = document.querySelector(".displayed-img");
 const thumbBar = document.querySelector(".thumb-bar");
 
